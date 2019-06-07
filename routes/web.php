b2b58147a9c03cb2ccfa1f02d23600baa03f282e
pham.tri.trung <FRAMGIA\pham.tri.trung@b121770-pc.framgia.com>
2019-06-07 15:56:33 +0700

    Implement SSR Sign In | Sign Up | Sign Out + Feature Test

--- routes/web.php
+++ routes/web.php
@@ -53,12 +53,44 @@ Route::get('cache/{storage}', function (string $storage) {
 //     Route::resource('user', 'Web\UserController');
 // });
 
+Route::group(['namespace' => 'Web'], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::group(['middleware' => ['auth']], function () {
+        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    });
+    // Login
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    // Register
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+    // Password reset
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+    // Email verify
+    Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+    Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+});
+
 Route::get('/client/{any}', function () {
     return view('client');
 })->where('any', '.*');
 
-/* Using the Cache Storage  */
->>>>>>> Install php memcached plugin
+/*
+|--------------------------------------------------------------------------
+| Using the Cache Storage
+|--------------------------------------------------------------------------
+|
+| Request to:
+| - /cached/redis
+| - /cached/memcached
+| - ...
+|
+*/
 Route::get('cache/{storage}', function (string $storage) {
     Cache::store($storage)->put('foo', 'baz', 60); // 1 Minute
     $value = Cache::store($storage)->get('foo');
